2c2f05b805489e0d3683efbfcc2dab1aca6b3895
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2024-03-01 17:24:30 +0100

    Update sample_new.py

--- scripts/sample_new.py
+++ scripts/sample_new.py
@@ -30,46 +30,6 @@ from torchvision import utils
 import math
 import clip
 
-######################################## STYLEX IMPORT
-
-
-import sys
-import h5py
-import numpy as np
-import shutil
-import pandas as pd
-
-from torch.utils.data import DataLoader
-import math
-import tqdm
-import random
-import imageio
-
-import multiprocessing
-from torchvision.utils import make_grid
-from PIL import Image
-import ast
-import torchvision
-from torchvision.datasets import ImageFolder
-from torchvision.transforms.functional import resize
-from torchvision.utils import save_image
-
-import requests
-from PIL import ImageDraw
-from PIL import ImageFont
-from io import BytesIO
-import IPython.display
-from IPython.display import HTML
-import matplotlib.pyplot as plt
-from shutil import copyfile
-import IPython.display as IPython_display
-
-os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
-
-from resnet_classifier import ResNet
-
-#########################################
-
 
 def main():
     time0 = time.time()
