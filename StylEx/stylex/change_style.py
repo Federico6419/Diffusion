c8551d98b7c51dffb3231aa11f545ff754a6f446
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2024-03-02 19:52:27 +0100

    Update change_style.py

--- StylEx/stylex/change_style.py
+++ StylEx/stylex/change_style.py
@@ -368,7 +368,7 @@ def create_latent(image):
   custom_dataset = CustomDataset(image, transform=transform)
 
   batch_size = 8
-  data_loader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
+  dataloader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
     
   #dataset = torch.utils.data.TensorDataset(image)
   #dataloader = DataLoader(dataset, batch_size=8, shuffle=False, transform=transform)
