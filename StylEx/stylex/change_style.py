fe58b3f126503103a7cbb19f87205495e14f94b7
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2024-03-02 12:03:30 +0100

    Update change_style.py

--- StylEx/stylex/change_style.py
+++ StylEx/stylex/change_style.py
@@ -30,7 +30,7 @@ import matplotlib.pyplot as plt
 from shutil import copyfile
 import IPython.display as IPython_display
 
-os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+os.environ["HDF5_USE_FILE_LOCKING"] = "TRUE"
 
 from resnet_classifier import ResNet
 
