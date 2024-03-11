974ddaa40b2d6a7435917f310fe66adcb373ba22
Francesco10998 <64406264+Francesco10998@users.noreply.github.com>
2024-03-11 14:13:51 +0100

    Update finetuning.py

--- sdg/finetuning.py
+++ sdg/finetuning.py
@@ -1,6 +1,9 @@
 """
 Finetuning a diffusion model using StylEx counterfactual.
 """
+################ general import ################
+import torch
+import torchvision.utils as vutils
 
 ############# diffusion import #################
 import argparse
@@ -22,53 +25,61 @@ from .fp16_util import MixedPrecisionTrainer
 
 
 
-
-
 """
 main code
 """
 def main():
   
-  model, diffusion = create_model_and_diffusion(
+    model, diffusion = create_model_and_diffusion(
       **args_to_dict(args, model_and_diffusion_defaults().keys())
-  )
-  model.to("cuda")
-
-  self.mp_trainer = MixedPrecisionTrainer(model=model, use_fp16=False,fp16_scale_growth=1e-3)
-  opt = AdamW(self.mp_trainer.master_params, lr=3e-4, weight_decay=0.0)
-  opt.to("cuda")
-
-  logger.log("creating data loader...")
-  original_data = load_data(
+    )
+    model.to("cuda")
+    
+    self.mp_trainer = MixedPrecisionTrainer(model=model, use_fp16=False,fp16_scale_growth=1e-3)
+    opt = AdamW(self.mp_trainer.master_params, lr=3e-4, weight_decay=0.0)
+    opt.to("cuda")
+    
+    logger.log("creating data loader...")
+    original_data = load_data(
       data_dir="../ref/counterfactual_dataset/original_images,
       batch_size=80,
       image_size=256,
       class_cond=False,
-  )
+    )
 
-
-  ############### load checkpoint #############
-  checkpoint = "../models/ffhq_p2.pt"
-  logger.log(f"loading model from checkpoint: {checkpoint}...")
-  model.load_state_dict(
+    counterfactual_data = load_data(
+          data_dir="../ref/counterfactual_dataset/counterfactual_images,
+          batch_size=80,
+          image_size=256,
+          class_cond=False,
+        )
+    
+    
+    ############### load checkpoint #############
+    checkpoint = "../models/ffhq_p2.pt"
+    logger.log(f"loading model from checkpoint: {checkpoint}...")
+    model.load_state_dict(
       dist_util.load_state_dict(
           checkpoint, map_location="cuda"
       )
-  )
-
-  opt.load_state_dict(
+    )
+    
+    opt.load_state_dict(
       dist_util.load_state_dict(
           checkpoint, map_location="cuda"
       )
-  )
-  #############################################
+    )
+    #############################################
 
-  schedule_sampler = create_named_schedule_sampler("uniform", diffusion)
-  
-  ################ precompute latents #####################
-  
+    schedule_sampler = create_named_schedule_sampler("uniform", diffusion)
 
+    ################ precompute latents #####################
+    latent = q_sample(self, original_data, 1000, noise=None)
 
+    # Salva il batch di immagini in un file per iterarle nel training
+    vutils.save_image(tensor_img_batch, '../latents/batch_images.png', nrow=80, normalize=True)
+
+    #################### training #########################
   
 
 
