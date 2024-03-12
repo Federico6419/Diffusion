988f21f69ae87da9f2a9658cb11d39388b6290a9
Francesco10998 <64406264+Francesco10998@users.noreply.github.com>
2024-03-12 16:52:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="sdg",
     py_modules=["sdg"],
-    install_requires=["blobfile>=1.0.5", "tqdm"],
+    install_requires=["blobfile>=1.0.5","torch", "tqdm"],
 )
