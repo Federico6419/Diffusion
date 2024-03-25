609e135448501c5cbfc2b6f8149f29a80df9975f
Francesco10998 <64406264+Francesco10998@users.noreply.github.com>
2024-03-25 14:03:27 +0100

    Update train_util_new.py

--- sdg/train_util_new.py
+++ sdg/train_util_new.py
@@ -1,8 +1,6 @@
 import copy
 import functools
 import os
-import time
-import glob
 
 import blobfile as bf
 import torch as th
@@ -10,14 +8,10 @@ import torch.distributed as dist
 from torch.nn.parallel.distributed import DistributedDataParallel as DDP
 from torch.optim import AdamW
 
-from .distributed import get_world_size, is_master
-from .distributed import master_only_print as print
+from . import dist_util, logger
 from .fp16_util import MixedPrecisionTrainer
 from .nn import update_ema
 from .resample import LossAwareSampler, UniformSampler
-from .logging import write_tb
-from .misc import to_cuda
-from . import logger
 
 # For ImageNet experiments, this was a good default value.
 # We found that the lg_loss_scale quickly climbed to
@@ -28,51 +22,59 @@ INITIAL_LOG_LOSS_SCALE = 20.0
 class TrainLoop:
     def __init__(
         self,
-        cfg,
+        *,
         model,
         diffusion,
-        data_train,
-        tb_log,
+        data,
+        batch_size,
+        microbatch,
+        lr,
+        ema_rate,
+        log_interval,
+        save_interval,
+        resume_checkpoint,
+        use_fp16=False,
+        fp16_scale_growth=1e-3,
         schedule_sampler=None,
+        weight_decay=0.0,
+        lr_anneal_steps=0,
     ):
-        self.tb_log = tb_log
         self.model = model
         self.diffusion = diffusion
-        self.data_train = data_train
-        self.batch_size = cfg.batch_size
-        self.microbatch = cfg.microbatch if cfg.microbatch > 0 else cfg.batch_size
-        self.lr = cfg.lr
+        self.data = data
+        self.batch_size = batch_size
+        self.microbatch = microbatch if microbatch > 0 else batch_size
+        self.lr = lr
         self.ema_rate = (
-            [cfg.ema_rate]
-            if isinstance(cfg.ema_rate, float)
-            else [float(x) for x in cfg.ema_rate.split(",")]
+            [ema_rate]
+            if isinstance(ema_rate, float)
+            else [float(x) for x in ema_rate.split(",")]
         )
-        self.log_interval = cfg.log_interval
-        self.save_interval = cfg.save_interval
-        self.resume_checkpoint = cfg.resume_checkpoint
-        self.use_fp16 = getattr(cfg, 'use_fp16', False)
-        self.fp16_scale_growth = getattr(cfg, 'fp16_scale_growth', 1e-3)
+        self.log_interval = log_interval
+        self.save_interval = save_interval
+        self.resume_checkpoint = resume_checkpoint
+        self.use_fp16 = use_fp16
+        self.fp16_scale_growth = fp16_scale_growth
         self.schedule_sampler = schedule_sampler or UniformSampler(diffusion)
-        self.weight_decay = getattr(cfg, 'weight_decay', 0.0)
-        self.lr_anneal_steps = getattr(cfg, 'lr_anneal_steps', 0)
-        self.logdir = getattr(cfg, 'logdir', 'logs/debug')
-        fp16_hyperparams = getattr(cfg, 'fp16_hyperparams', 'openai')
-        if self.use_fp16:
-            if fp16_hyperparams == 'pytorch':
-                self.scaler = th.cuda.amp.GradScaler(init_scale=65536.0, growth_factor=2.0, backoff_factor=0.5, growth_interval=2000, enabled=True)
-            elif fp16_hyperparams == 'openai':
-                self.scaler = th.cuda.amp.GradScaler(init_scale=2**20 * 1.0, growth_factor=2**0.001, backoff_factor=0.5, growth_interval=1, enabled=True)
+        self.weight_decay = weight_decay
+        self.lr_anneal_steps = lr_anneal_steps
 
         self.step = 0
         self.resume_step = 0
-        self.global_batch = self.batch_size * get_world_size()
+        self.global_batch = self.batch_size * dist.get_world_size()
 
-        self.sync_cuda = True  # th.cuda.is_available()
+        self.sync_cuda = th.cuda.is_available()
 
         self._load_and_sync_parameters()
+        self.mp_trainer = MixedPrecisionTrainer(
+            model=self.model,
+            use_fp16=self.use_fp16,
+            fp16_scale_growth=fp16_scale_growth,
+        )
 
-        self.params = list(self.model.parameters())
-        self.opt = AdamW(self.params, lr=self.lr, weight_decay=self.weight_decay)
+        self.opt = AdamW(
+            self.mp_trainer.master_params, lr=self.lr, weight_decay=self.weight_decay
+        )
         if self.resume_step:
             self._load_optimizer_state()
             # Model was resumed, either due to a restart or a checkpoint
@@ -82,123 +84,109 @@ class TrainLoop:
             ]
         else:
             self.ema_params = [
-                copy.deepcopy(self.params)
+                copy.deepcopy(self.mp_trainer.master_params)
                 for _ in range(len(self.ema_rate))
             ]
 
-        self.use_ddp = th.cuda.is_available() and th.distributed.is_available() and dist.is_initialized()
-        if self.use_ddp:
+        if th.cuda.is_available():
+            self.use_ddp = True
             self.ddp_model = DDP(
                 self.model,
-                device_ids=[cfg.local_rank],
-                output_device=cfg.local_rank,
-                bucket_cap_mb=128,
+                device_ids=[dist_util.dev()],
+                output_device=dist_util.dev(),
                 broadcast_buffers=False,
+                bucket_cap_mb=128,
                 find_unused_parameters=False,
             )
         else:
-            print(
-                "Single GPU Training without DistributedDataParallel. "
-            )
+            if dist.get_world_size() > 1:
+                logger.warn(
+                    "Distributed training requires CUDA. "
+                    "Gradients will not be synchronized properly!"
+                )
+            self.use_ddp = False
             self.ddp_model = self.model
 
     def _load_and_sync_parameters(self):
-        resume_checkpoint = find_resume_checkpoint(self.logdir) or self.resume_checkpoint
+        resume_checkpoint = find_resume_checkpoint() or self.resume_checkpoint
 
         if resume_checkpoint:
             self.resume_step = parse_resume_step_from_filename(resume_checkpoint)
-            print(f"loading model from checkpoint: {resume_checkpoint}...")
-            checkpoint = th.load(resume_checkpoint, map_location=lambda storage, loc: storage)
-            self.model.load_state_dict(checkpoint)
+            if dist.get_rank() == 0:
+                logger.log(f"loading model from checkpoint: {resume_checkpoint}...")
+                self.model.load_state_dict(
+                    dist_util.load_state_dict(
+                        resume_checkpoint, map_location=dist_util.dev()
+                    )
+                )
+
+        dist_util.sync_params(self.model.parameters())
 
     def _load_ema_parameters(self, rate):
-        ema_params = copy.deepcopy(self.params)
+        ema_params = copy.deepcopy(self.mp_trainer.master_params)
 
-        main_checkpoint = find_resume_checkpoint(self.logdir) or self.resume_checkpoint
+        main_checkpoint = find_resume_checkpoint() or self.resume_checkpoint
         ema_checkpoint = find_ema_checkpoint(main_checkpoint, self.resume_step, rate)
         if ema_checkpoint:
-            print(f"loading EMA from checkpoint: {ema_checkpoint}...")
-            state_dict = th.load(ema_checkpoint, map_location=lambda storage, loc: storage)
-            ema_params = to_cuda([state_dict[name] for name, _ in self.model.named_parameters()])
+            if dist.get_rank() == 0:
+                logger.log(f"loading EMA from checkpoint: {ema_checkpoint}...")
+                state_dict = dist_util.load_state_dict(
+                    ema_checkpoint, map_location=dist_util.dev()
+                )
+                ema_params = self.mp_trainer.state_dict_to_master_params(state_dict)
 
+        dist_util.sync_params(ema_params)
         return ema_params
 
     def _load_optimizer_state(self):
-        main_checkpoint = find_resume_checkpoint(self.logdir) or self.resume_checkpoint
+        main_checkpoint = find_resume_checkpoint() or self.resume_checkpoint
         opt_checkpoint = bf.join(
             bf.dirname(main_checkpoint), f"opt{self.resume_step:06}.pt"
         )
         if bf.exists(opt_checkpoint):
-            print(f"loading optimizer state from checkpoint: {opt_checkpoint}")
-            checkpoint = th.load(opt_checkpoint, map_location=lambda storage, loc: storage)
-            self.opt.load_state_dict(checkpoint)
-
-    def _compute_norms(self, grad_scale=1.0):
-        grad_norm = 0.0
-        param_norm = 0.0
-        for p in self.params:
-            with th.no_grad():
-                param_norm += th.norm(p, p=2, dtype=th.float32) ** 2
-                if p.grad is not None:
-                    grad_norm += th.norm(p.grad, p=2, dtype=th.float32) ** 2
-        return th.sqrt(grad_norm) / grad_scale, th.sqrt(param_norm)
+            logger.log(f"loading optimizer state from checkpoint: {opt_checkpoint}")
+            state_dict = dist_util.load_state_dict(
+                opt_checkpoint, map_location=dist_util.dev()
+            )
+            self.opt.load_state_dict(state_dict)
 
     def run_loop(self):
-        time0 = time.time()
         while (
             not self.lr_anneal_steps
             or self.step + self.resume_step < self.lr_anneal_steps
         ):
-            print('***step %d  ' % (self.step + self.resume_step), end='')
-            batch, cond = next(self.data_train)
-            time2 = time.time()
-            if self.use_fp16:
-                self.run_step_amp(batch, cond)
-            else:
-                self.run_step(batch, cond)
-            if self.step % self.log_interval == 0 and is_master():
-                self.tb_log.flush()
+            batch, cond = next(self.data)
+            self.run_step(batch, cond)
+            if self.step % self.log_interval == 0:
+                logger.dumpkvs()
             if self.step % self.save_interval == 0:
                 self.save()
                 # Run for a finite amount of time in integration tests.
                 if os.environ.get("DIFFUSION_TRAINING_TEST", "") and self.step > 0:
                     return
-            num_samples = (self.step + self.resume_step + 1) * self.global_batch
-            time1 = time.time()
-            if is_master():
-                self.tb_log.add_scalar('status/step', self.step+self.resume_step, self.step + self.resume_step)
-                self.tb_log.add_scalar('status/samples', num_samples, self.step + self.resume_step)
-                self.tb_log.add_scalar('time/time_per_iter', time1-time0, self.step + self.resume_step)
-                self.tb_log.add_scalar('time/data_time_per_iter', time2-time0, self.step + self.resume_step)
-                self.tb_log.add_scalar('time/model_time_per_iter', time1-time2, self.step + self.resume_step)
-                self.tb_log.add_scalar('status/lr', self.lr, self.step + self.resume_step)
-            print('lr: %f  ' % self.lr, end='')
-            print('samples: %d  ' % num_samples, end='')
-            print('data time: %f  ' % (time2-time0), end='')
-            print('model time: %f  ' % (time1-time2), end='')
-            print('')
             self.step += 1
-            time0 = time1
         # Save the last checkpoint if it wasn't already saved.
         if (self.step - 1) % self.save_interval != 0:
             self.save()
 
     def run_step(self, batch, cond):
         self.forward_backward(batch, cond)
-        self.opt.step()
-        self._update_ema()
+        took_step = self.mp_trainer.optimize(self.opt)
+        if took_step:
+            self._update_ema()
         self._anneal_lr()
+        self.log_step()
 
     def forward_backward(self, batch, cond):
-        self.opt.zero_grad()
+        self.mp_trainer.zero_grad()
         for i in range(0, batch.shape[0], self.microbatch):
-            micro = to_cuda(batch[i : i + self.microbatch])
+            micro = batch[i : i + self.microbatch].to(dist_util.dev())
             micro_cond = {
-                k: to_cuda(v[i : i + self.microbatch])
+                k: v[i : i + self.microbatch].to(dist_util.dev())
                 for k, v in cond.items()
             }
             last_batch = (i + self.microbatch) >= batch.shape[0]
-            t, weights = self.schedule_sampler.sample(micro.shape[0], 'cuda')
+            t, weights = self.schedule_sampler.sample(micro.shape[0], dist_util.dev())
 
             compute_losses = functools.partial(
                 self.diffusion.training_losses,
@@ -220,64 +208,14 @@ class TrainLoop:
                 )
 
             loss = (losses["loss"] * weights).mean()
-            if self.step % 10 == 0:
-                log_loss_dict(
-                    self.diffusion, t, {k: v * weights for k, v in losses.items()},
-                    self.tb_log, self.step + self.resume_step
-                )
-            loss.backward()
-
-    def run_step_amp(self, batch, cond):
-        self.forward_backward_amp(batch, cond)
-        self.scaler.step(self.opt)
-        self.scaler.update()
-        self._update_ema()
-        self._anneal_lr()
-
-    def forward_backward_amp(self, batch, cond):
-        self.opt.zero_grad()
-        for i in range(0, batch.shape[0], self.microbatch):
-            micro = to_cuda(batch[i : i + self.microbatch])
-            micro_cond = {
-                k: to_cuda(v[i : i + self.microbatch])
-                for k, v in cond.items()
-            }
-            last_batch = (i + self.microbatch) >= batch.shape[0]
-            t, weights = self.schedule_sampler.sample(micro.shape[0], 'cuda')
-
-            with th.cuda.amp.autocast(True):
-                compute_losses = functools.partial(
-                    self.diffusion.training_losses,
-                    self.ddp_model,
-                    micro,
-                    t,
-                    model_kwargs=micro_cond,
-                )
-
-                if last_batch or not self.use_ddp:
-                    losses = compute_losses()
-                else:
-                    with self.ddp_model.no_sync():
-                        losses = compute_losses()
-
-                if isinstance(self.schedule_sampler, LossAwareSampler):
-                    self.schedule_sampler.update_with_local_losses(
-                        t, losses["loss"].detach()
-                    )
-
-                loss = (losses["loss"] * weights).mean()
-                if self.step % 10 == 0:
-                    log_loss_dict(
-                        self.diffusion, t, {k: v * weights for k, v in losses.items()},
-                        self.tb_log, self.step + self.resume_step
-                    )
-                self.scaler.scale(loss).backward()
-
-
+            log_loss_dict(
+                self.diffusion, t, {k: v * weights for k, v in losses.items()}
+            )
+            self.mp_trainer.backward(loss)
 
     def _update_ema(self):
         for rate, params in zip(self.ema_rate, self.ema_params):
-            update_ema(params, self.params, rate=rate)
+            update_ema(params, self.mp_trainer.master_params, rate=rate)
 
     def _anneal_lr(self):
         if not self.lr_anneal_steps:
@@ -292,33 +230,29 @@ class TrainLoop:
         logger.logkv("samples", (self.step + self.resume_step + 1) * self.global_batch)
 
     def save(self):
-        def save_checkpoint(rate):
-            print(f"saving model {rate}...")
-            if is_master():
+        def save_checkpoint(rate, params):
+            state_dict = self.mp_trainer.master_params_to_state_dict(params)
+            if dist.get_rank() == 0:
+                logger.log(f"saving model {rate}...")
                 if not rate:
                     filename = f"model{(self.step+self.resume_step):06d}.pt"
                 else:
                     filename = f"ema_{rate}_{(self.step+self.resume_step):06d}.pt"
-                with bf.BlobFile(bf.join(self.logdir, filename), "wb") as f:
-                    th.save(self.model.state_dict(), f)
+                with bf.BlobFile(bf.join(get_blob_logdir(), filename), "wb") as f:
+                    th.save(state_dict, f)
 
-        save_checkpoint(0)
+        save_checkpoint(0, self.mp_trainer.master_params)
         for rate, params in zip(self.ema_rate, self.ema_params):
-            save_checkpoint(rate)
+            save_checkpoint(rate, params)
 
-        if is_master():
+        if dist.get_rank() == 0:
             with bf.BlobFile(
-                bf.join(self.logdir, f"opt{(self.step+self.resume_step):06d}.pt"),
+                bf.join(get_blob_logdir(), f"opt{(self.step+self.resume_step):06d}.pt"),
                 "wb",
             ) as f:
                 th.save(self.opt.state_dict(), f)
 
-        if is_master() and self.use_fp16:
-            with bf.BlobFile(
-                bf.join(self.logdir, f"scaler{(self.step+self.resume_step):06d}.pt"),
-                "wb",
-            ) as f:
-                th.save(self.scaler.state_dict(), f)
+        dist.barrier()
 
 
 def parse_resume_step_from_filename(filename):
@@ -342,14 +276,10 @@ def get_blob_logdir():
     return logger.get_dir()
 
 
-def find_resume_checkpoint(logdir):
+def find_resume_checkpoint():
     # On your infrastructure, you may want to override this to automatically
     # discover the latest checkpoint on your blob storage, etc.
-    models = sorted(glob.glob(os.path.join(logdir, 'model*.pt')))
-    if len(models) >= 1:
-        return models[-1]
-    else:
-        return None
+    return None
 
 
 def find_ema_checkpoint(main_checkpoint, step, rate):
@@ -362,15 +292,10 @@ def find_ema_checkpoint(main_checkpoint, step, rate):
     return None
 
 
-def log_loss_dict(diffusion, ts, losses, tb_log, step, prefix='loss'):
+def log_loss_dict(diffusion, ts, losses):
     for key, values in losses.items():
-        write_tb(tb_log, f"{prefix}/{key}", values, step)
-        quartile_list = [[] for cnt in range(4)]
+        logger.logkv_mean(key, values.mean().item())
+        # Log the quantiles (four quartiles, in particular).
         for sub_t, sub_loss in zip(ts.cpu().numpy(), values.detach().cpu().numpy()):
             quartile = int(4 * sub_t / diffusion.num_timesteps)
-            quartile_list[quartile].append(sub_loss)
-        for cnt in range(4):
-            if len(quartile_list[cnt]) != 0:
-                write_tb(tb_log, f"{prefix}/{key}_q{cnt}", sum(quartile_list[cnt])/len(quartile_list[cnt]), step)
-            else:
-                write_tb(tb_log, f"{prefix}/{key}_q{cnt}", 0.0, step)
+            logger.logkv_mean(f"{key}_q{quartile}", sub_loss)
